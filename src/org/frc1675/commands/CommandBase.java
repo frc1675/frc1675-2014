b5b7d89cb27a98df0c927aedbdc5db2d2fd9ea82
Alex <Alex@Lulewicz-laptop>
2014-01-18 11:55:17 -0600

    Alex's test comment removed

--- src/org/frc1675/commands/CommandBase.java
+++ src/org/frc1675/commands/CommandBase.java
@@ -9,7 +9,7 @@ import org.frc1675.subsystems.Roller;
 import org.frc1675.subsystems.Shoulder;
 
 /**
- * Alex was here
+
  * The base for all commands. All atomic commands should subclass CommandBase.
  * CommandBase stores creates and stores each control system. To access a
  * subsystem elsewhere in your code in your code use CommandBase.exampleSubsystem
