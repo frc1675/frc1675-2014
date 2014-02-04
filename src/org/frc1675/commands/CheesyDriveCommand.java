f6e047b50ac0f6acf8e09b2759d7627056bad4f8
Mannkijaaduu <mannkijaaduu@gmail.com>
2014-02-03 19:07:42 -0600

    Added Cheesy Drive. :)

---
+++ src/org/frc1675/commands/CheesyDriveCommand.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.frc1675.commands;
+
+
+/**
+ *
+ * @author hemilia_bedilia
+ */
+public class CheesyDriveCommand extends CommandBase {
+    
+    public CheesyDriveCommand() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+        requires(driveBase);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+        double forward = oi.getDriverLeftY();
+        double turn = oi.getDriverRightY();
+        
+        double left;
+        double right;
+        
+        left = forward - turn;
+        right = forward + turn;
+        double surplus = 0;
+        if (left > 1){
+            surplus = left - 1;
+            right = right - surplus;
+        } else if (right > -1){
+            surplus = right - 1;
+            left = left - surplus;
+        } else if (left < -1){
+            surplus = -1 - left;
+            right = right + surplus;
+        } else if (right < -1){
+            surplus = -1 - right;
+            left = left + surplus;
+        }
+        
+        driveBase.setLeftMotors(left);
+        driveBase.setRightMotors(right);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
